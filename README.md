9c6de3387081527d11bfebd4a963a00dee5ca356
robvanvolt <robvanvolt@gmail.com>
2024-02-28 20:37:26 -0800

    updated guide in instructions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instructions
+
+1. Put all images in the `images` folder of the corresponding project
+2. Update the instructions in the `guide.json` of the corresponding project
+3. Run the script `finder.py` to find all the images that are not being used in the `guide.json`
+4. Upload your changes to the repository
---
+++ finder.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+def update_image_list(directory):
+    # Get list of all files in directory
+    all_files = os.listdir(os.path.join(directory, "images"))
+
+    # Filter list to include only image files
+    image_files = [f for f in all_files if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff'))]
+
+    # Open the guide.json file
+    with open(os.path.join(directory, 'guide.json'), 'r+') as file:
+        # Load the JSON data
+        data = json.load(file)
+
+        # Modify the "images" key
+        data['images'] = image_files
+
+        # Write the modified JSON data back to the file
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+        
+if __name__ == '__main__':
+    update_image_list('./projects/p1')
